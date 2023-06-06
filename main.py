816d53bc75a7a5edc857729d8c593ecf7b836887
Abdullah-Alshkarh <129951142+Abdullah-Alshkarh@users.noreply.github.com>
2023-06-06 06:50:52 +0300

    1

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 import tkinter
 from tkinter import *
 from PIL import Image, ImageTk
+import random
+
 
 def faceBox(faceNet,frame):
     frameHeight=frame.shape[0]
@@ -22,7 +24,8 @@ def faceBox(faceNet,frame):
     return frame, bboxs
 
 
-
+prev_age=""
+prev_gender=""
 root = Tk()
 root.geometry("1000x1000")
 
@@ -46,7 +49,7 @@ ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)
 genderList = ['Male', 'Female']
 
 
-video=cv2.VideoCapture("Untitled video - Made with Clipchamp (11).mp4")  # 0 for webcam - you also can put a video title insted - "video.mp4"
+video=cv2.VideoCapture("Untitled video - Made with Clipchamp (17).mp4")  # 0 for webcam - you also can put a video title insted - "video.mp4"
 
 padding=20
 gender=''
@@ -72,10 +75,19 @@ while True:
         cv2.putText(frame, label, (bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255), 2,cv2.LINE_AA)
     cv2.imshow("Age-Gender",frame)   #this show the live webcam to the screen (not requried)
 
+
+
+
+
     if gender == 'Male' or gender == 'Female':
         try:
-            source= 'gender/'+gender+'/'+age+'.png'
-            img = Image.open(source)
+            
+            if(age!=prev_age or gender!=prev_gender):
+                num1 = str(random.randint(1, 3))
+                source= 'gender/'+gender+'/'+age+num1+'.png'
+                img = Image.open(source)
+            prev_age=age
+            prev_gender=gender
         except:
             print(source+'dosent exist')
         
